468575424105fb277ad64ae2e59437dd8a8c6375
CalebeNavarro <calebe.snavarro@gmail.com>
2022-01-10 19:41:20 -0300

    feat: .env PASSWORD_AUTH

--- app/configs/env_configs.py
+++ app/configs/env_configs.py
@@ -1,5 +1,9 @@
 from flask import Flask
 
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 def init_app(app: Flask):
     app.config["JSON_SORT_KEYS"] = False
-    app.config["JWT_SECRET_KEY"] = "Kenzi3"
+    app.config["JWT_SECRET_KEY"] = os.environ.get("PASSWORD_AUTH")

487ba95f978bdbecf25caece29c0f1abaa24f191
CalebeNavarro <calebe.snavarro@gmail.com>
2022-01-16 18:08:08 -0300

    feat: readme.md

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask, request, jsonify
 from app.configs import database, env_configs, jwt
 from app.routes import api_blueprint
-from flask_cors import CORS
 
 def create_app():
   app = Flask(__name__)
-  CORS(app, origins=["http://localhost:3001", "https://codewars-app-calebekenzie.vercel.app"])
+  
   env_configs.init_app(app)
   database.init_app(app)
   jwt.init_app(app)
--- app/configs/env_configs.py
+++ app/configs/env_configs.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
 def init_app(app: Flask):
+    CORS(app, origins=["http://localhost:3001", "https://codewars-app-calebekenzie.vercel.app"])
     app.config["JSON_SORT_KEYS"] = False
     app.config["JWT_SECRET_KEY"] = os.environ.get("PASSWORD_AUTH")

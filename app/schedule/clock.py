c6c5bfb3991cd2f3acdd82cc4f38abbd4e6d0ff1
CalebeNavarro <calebe.snavarro@gmail.com>
2022-02-08 11:36:11 -0300

    fix: clock.py

--- app/schedule/clock.py
+++ app/schedule/clock.py
@@ -1,26 +1,16 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import os
-from pytz import utc
 
-
-def job_function():
-    os.system("flask routine codewars")
 
 sched = BlockingScheduler()
 
-# Schedules job_function to be run on the third Friday
-# of June, July, August, November and December at 00:00, 01:00, 02:00 and 03:00
-sched.add_job(job_function, 'cron', hour='18', timezone=utc)
+
+@sched.scheduled_job('cron', hour='12')
+def scheduled_job():
+    os.system("flask routine codewars")
+
+@sched.scheduled_job('interval', minutes=60)
+def timed_job():
+    os.system("flask routine codewars")
 
 sched.start()
-
-
-# @sched.scheduled_job('cron', hour='12', timezone=utc)
-# def scheduled_job():
-#     os.system("flask routine codewars")
-
-# @sched.scheduled_job('interval', minutes=60)
-# def timed_job():
-#     os.system("flask routine codewars")
-
-# sched.start()
--- app/schedule/codewars.py
+++
@@ -1,8 +0,0 @@
-from app.model.codewars_model import Codewars
-
-
-def execute_codewars():
-  print("beginning")
-  r = Codewars.update_users_and_enablers()
-  print(r)
-  print("finish")

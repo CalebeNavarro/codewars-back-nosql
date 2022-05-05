e32c72cf6eddb21a11bb5b627cea5c2d82658bed
CalebeNavarro <calebe.snavarro@gmail.com>
2022-05-05 11:51:58 -0300

    feat: .env.example

--- app/configs/env_configs.py
+++ app/configs/env_configs.py
@@ -6,6 +6,6 @@ import os
 load_dotenv()
 
 def init_app(app: Flask):
-    CORS(app, origins=["http://localhost:3000", "http://127.0.0.1:5000", "https://codewars-app-calebekenzie.vercel.app", "https://codewars-kenzie-xi.vercel.app"])
+    CORS(app, origins=["https://codewars-kenzie-xi.vercel.app"])
     app.config["JSON_SORT_KEYS"] = False
     app.config["JWT_SECRET_KEY"] = os.environ.get("PASSWORD_AUTH")
--- app/controllers/codewars_controllers.py
+++ app/controllers/codewars_controllers.py
@@ -1,5 +1,5 @@
 import requests
-import asyncio
+
 
 class CodewarsControllers():
   def __init__(self) -> None:
